6df25e86dd46f6a808fac96f812f035042e5ceba
TuperWar2.0 <louison.bednarowicz@outlook.com>
2024-04-06 21:30:11 +0200

    Paramétrage de docusaurus.config.json et ajout de logos. Suppression des docs

---
+++ INSAstuces/docusaurus.config.js
@@ -0,0 +1,197 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// math modules
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'INS\'Astuces HdF',
+  tagline: 'Le site pour les étudiants ingénieurs de l\'INSA Hauts-de-France',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://astuces.insahdf.fr',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: './insa.sh', // Usually your GitHub org/user name.
+  projectName: 'INS\'Astuces', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath:'/', // change the root url of the docs folder
+          sidebarPath: './sidebars.js',
+          remarkPlugins: [remarkMath], // ajout des modules pour le latex aux fichiers docs
+          rehypePlugins: [rehypeKatex], // ajout des modules pour le latex aux fichiers docs
+
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false, // disable the blog -> to turn it back on, just delete this line and uncomment the next block
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'INS\'Astuces HdF',
+        logo: {
+          alt: 'Logo du site INS\'Astuces',
+          src: 'img/logos/logo2.svg',
+        },
+        items: [
+          {
+            type:"dropdown",
+            sidebarId: 'docsSidebar',
+            position: 'left',
+            label: 'Futur étudiant',
+            items: [
+              {
+                label: '🗺️ Plan du campus & Repères',
+                to: '/' // 'to' for an internal link, 'href' for an external link with an 'open in a new tab' icon
+              },
+              {
+                label: '🏠 Logement',
+                to: '/'
+              },
+              {
+                label: '🚍 Transports',
+                to: '/'
+              },
+              {
+                label: '📚 Starter Packs',
+                to: '/'
+              },
+              {
+                label: '📝 Admissions',
+                to: '/'
+              },
+            ]
+
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+
+
+    }),
+    stylesheets: [
+      // feuilles de styles pour latex -> supprime le doublon en version texte des equations latex (katex)
+      {
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+        type: 'text/css',
+        integrity:
+          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        crossorigin: 'anonymous',
+      },
+    ],
+};
+
+export default config;
