d9b64797850c49bccbcfbc13074012454b4cbaae
TuperWar2.0 <louison.bednarowicz@outlook.com>
2024-04-06 03:05:20 +0200

    really adds color theme auto detection

--- INS'Astuces/docusaurus.config.js
+++ INS'Astuces/docusaurus.config.js
@@ -151,7 +151,7 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
   
     }),
